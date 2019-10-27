29b9733d0e2a06e27b75767ba2fbebfe23196136
Maria <Maria@MARIA-PC>
2019-10-27 11:03:05 +0200

    update

--- sapt3tema/Program.cs
+++ sapt3tema/Program.cs
@@ -10,8 +10,31 @@ namespace sapt3tema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello word!!");
+            int an;
+            Console.WriteLine("Introduceti un an:");
+            an = int.Parse(Console.ReadLine());
+            if ((an % 400) == 0)
+            {
+                Console.WriteLine("Anul {0} este an bisect", an);
+                Console.ReadLine();
+            }
+            else if ((an % 100) == 0)
+            {
+                Console.WriteLine("Anul {0}  nu este an bisect", an);
+                Console.ReadLine();
+            }
+            else if((an% 4) == 0)
+            {
+                Console.WriteLine("Anul {0} este an bisect", an);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Anul {0} nu este an bisect", an);
+                Console.ReadLine();
+            }
         }
-
+  
     }
+   
 }
