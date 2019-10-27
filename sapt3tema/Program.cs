381e663472777273e2f554943b2cb9fb6ad85784
Maria <Maria@MARIA-PC>
2019-10-27 12:50:57 +0200

    update

--- sapt3tema/Program.cs
+++ sapt3tema/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,36 @@ namespace sapt3tema
     class Program
     {
         static void Main(string[] args)
+        {
+            //AnBisesct();
+
+            string text1 = System.IO.File.ReadAllText(@"C:\Users\Maria\source\repos\week3ora22\File1.txt");
+            
+            string[] linii = System.IO.File.ReadAllLines(@"C:\Users\Maria\source\repos\week3ora22\File1.txt");
+
+            int valoare1 = int.Parse(linii[0]);
+            int valoare2 = int.Parse(linii[1]);
+            int suma = valoare1 + valoare2;
+         
+
+            Console.WriteLine("Continutul fiserului text este :");
+            
+            foreach (string line in linii)
+            {
+                
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.WriteLine(suma + " este suma numerelor");
+
+            using (System.IO.StreamWriter writer = new StreamWriter(@"C:\Users\Maria\source\repos\week3ora22\File1.txt", true))
+            {
+                writer.WriteLine($"numarul {valoare1} adunat cu numarul {valoare2} are suma {suma}");
+            }
+            Console.ReadLine();
+        }
+
+        private static void AnBisesct()
         {
             int an;
             Console.WriteLine("Introduceti un an:");
@@ -23,7 +54,7 @@ namespace sapt3tema
                 Console.WriteLine("Anul {0}  nu este an bisect", an);
                 Console.ReadLine();
             }
-            else if((an% 4) == 0)
+            else if ((an % 4) == 0)
             {
                 Console.WriteLine("Anul {0} este an bisect", an);
                 Console.ReadLine();
@@ -34,7 +65,6 @@ namespace sapt3tema
                 Console.ReadLine();
             }
         }
-  
     }
    
 }
