88df7c667d3c7051ced07f8e91a630f7ffbc10d1
Maria <Maria@MARIA-PC>
2019-10-27 14:35:05 +0200

    update

--- sapt3tema/Program.cs
+++ sapt3tema/Program.cs
@@ -11,22 +11,89 @@ namespace sapt3tema
     {
         static void Main(string[] args)
         {
-            //AnBisesct();
+            //5.3 Tema1
 
+            //int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            //RotateArrByAPivot(arr, 5);
+            //TotalNumberBits();
+
+
+            //6.3Tema 2
+
+            //AnBisesct();
+            //SumInFile();
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void TotalNumberBits()
+        {
+            int i;
+            Console.WriteLine("Introduceti un numar:");
+            i = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Suma bitiilor numarului {i} este:");
+            Console.WriteLine(countBits(i));
+        }
+
+        static int countBits(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                count += n & 1;
+                n >>= 1;
+            }
+            return count;
+        }
+        static void RotateArrByAPivot(int[] arr, int nr)
+        {
+            int[] newArr = new int[arr.Length];
+            int i;
+            int j = 0;
+            for (i = nr + 1; i < arr.Length; i++)
+            {
+                newArr[j] = arr[i];
+                j++;
+            }
+            for (int k = 0; k < nr; k++)
+            {
+                if (k == 0)
+                {
+                    newArr[j] = arr[nr];
+                    j++;
+                }
+                newArr[j] = arr[k];
+                j++;
+            }
+            PrintArrays(newArr);
+        }
+        static void PrintArrays(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + ", ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------");
+        }
+        private static void SumInFile()
+        {
             string text1 = System.IO.File.ReadAllText(@"C:\Users\Maria\source\repos\week3ora22\File1.txt");
-            
+
             string[] linii = System.IO.File.ReadAllLines(@"C:\Users\Maria\source\repos\week3ora22\File1.txt");
 
             int valoare1 = int.Parse(linii[0]);
             int valoare2 = int.Parse(linii[1]);
             int suma = valoare1 + valoare2;
-         
+
 
             Console.WriteLine("Continutul fiserului text este :");
-            
+
             foreach (string line in linii)
             {
-                
+
                 Console.WriteLine("\t" + line);
             }
 
